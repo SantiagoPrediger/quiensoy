05780824788db2d3f922bde48fa3dbfe818524cf
Santiago Prediger <santiago.prediger@gmail.com>
2022-10-10 20:24:30 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('Hola Natasha')
+
+let miNombre = 'Santiago'
+
+console.log(miNombre)
+
+
+let nombre = prompt('Ingrese su nombre')
+let apellido = prompt('Ingrese su apellido')
+let ciudad = prompt('Ingrese su ciudad Natal')
+let hobbie = prompt('Ingrese un hobbie')
+let nacimiento = prompt('En que año naciste')
+
+
+alert('Hola mi nombre es ' + nombre + ' ' + apellido + ' tengo' + ' ' + ( 2022 - nacimiento ) + ' ' + 'años, soy de la ciudad de ' + ciudad + ' ' + 'y me gusta ' + hobbie + ' en mis tiempos libres.'  )
+
